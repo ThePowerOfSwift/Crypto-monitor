3bb538ee93f3c652ec28e252e96f5053fc7f8fce
valic <valic.my@gmail.com>
2017-08-01 10:31:12 +0300

    Fix

--- Cryptocurrency monitor widget/TodayViewController.swift
+++ Cryptocurrency monitor widget/TodayViewController.swift
@@ -13,10 +13,23 @@ class TodayViewController: UIViewController, NCWidgetProviding {
     
     @IBOutlet weak var tableView: UITableView!
 
-        
+
+    var id = [String]()
+    
+    let x = ["ss", "sdf", "freger"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view from its nib.
+        
+        let keyStore = NSUbiquitousKeyValueStore ()
+        print(keyStore.array(forKey: "id") as! [String])
+        id = keyStore.array(forKey: "id") as! [String]
+        
+        extensionContext?.widgetLargestAvailableDisplayMode = .expanded
+        self.preferredContentSize.height = 200
+        self.tableView.reloadData()
+        
     }
     
     override func didReceiveMemoryWarning() {
@@ -31,7 +44,40 @@ class TodayViewController: UIViewController, NCWidgetProviding {
         // If there's no update required, use NCUpdateResult.NoData
         // If there's an update, use NCUpdateResult.NewData
         
+        let keyStore = NSUbiquitousKeyValueStore ()
+        id = keyStore.array(forKey: "id") as! [String]
+        self.tableView.reloadData()
+        
+        
         completionHandler(NCUpdateResult.newData)
     }
+    func widgetMarginInsets(forProposedMarginInsets defaultMarginInsets: UIEdgeInsets) -> (UIEdgeInsets) {
+        return UIEdgeInsets.zero
+    }
     
+    func widgetActiveDisplayModeDidChange(_ activeDisplayMode: NCWidgetDisplayMode, withMaximumSize maxSize: CGSize) {
+        if activeDisplayMode == .expanded {
+            let height = 44.0 * Float(id.count)
+            preferredContentSize = CGSize(width: maxSize.width, height: CGFloat(height))
+        }
+        else if activeDisplayMode == .compact {
+            preferredContentSize = maxSize
+        }
+    }
+    
+    // MARK: - TableView Data Source
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return id.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAtIndexPath indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "tableViewCellIdentifier", for: indexPath)
+        
+        let item = id[indexPath.row]
+        cell.textLabel?.text = item
+        cell.textLabel?.textColor = UIColor.black
+        
+        return cell
+    }
 }
---
+++ CryptocurrencyRequest/CryptocurrencyRequest.h
@@ -0,0 +1,19 @@
+//
+//  CryptocurrencyRequest.h
+//  CryptocurrencyRequest
+//
+//  Created by Mialin Valentin on 01.08.17.
+//  Copyright © 2017 Mialin Valentyn. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for CryptocurrencyRequest.
+FOUNDATION_EXPORT double CryptocurrencyRequestVersionNumber;
+
+//! Project version string for CryptocurrencyRequest.
+FOUNDATION_EXPORT const unsigned char CryptocurrencyRequestVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CryptocurrencyRequest/PublicHeader.h>
+
+
